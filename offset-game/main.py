6c553363616487434c94e7ca9b86c9e5a018488a
Hemu <hemanthm2277@gmail.com>
2019-11-09 22:08:44 -0500

    Added parameter server in ray

--- offset-game/main.py
+++ offset-game/main.py
@@ -1,7 +1,10 @@
-import time
 import yaml
 from pathlib import Path
+import time
 
+import ray
+
+from server.parameters import ParameterServer
 from envs.environments import Benning
 
 from utils import skip_run
@@ -12,15 +15,28 @@ config = yaml.load(open(str(config_path)), Loader=yaml.SafeLoader)
 
 with skip_run('run', 'learning tactic') as check, check():
 
-    env = Benning(config)
+    # Initiate ray
+    if not ray.is_initialized():
+        ray.init(num_cpus=4)
+
+    # Instantiate parameter server
+    ps = ParameterServer.remote()
+
+    # Instantiate environment
+    env = Benning.remote(config)
+
     # ['n_robots', 'primitive', 'target_node_id', 0, 0, 0]
-    net_output_1 = [[20, 1, 38, 0, 0, 0], [10, 1, 39, 0, 0, 0],
-                    [20, 1, 40, 0, 0, 0], [12, 1, 15, 0, 0, 0],
-                    [9, 1, 12, 0, 0, 0], [4, 1, 11, 0, 0, 0]]
-    start = time.time()
-    for j in range(1):
+    net_output = [[20, 1, 38, 0, 0, 0], [10, 1, 39, 0, 0, 0],
+                  [20, 1, 40, 0, 0, 0], [12, 1, 15, 0, 0, 0],
+                  [9, 1, 12, 0, 0, 0], [4, 1, 11, 0, 0, 0]]
+
+    start_time = time.time()
+    for j in range(10):
         print(j)
-        _, _, done = env.step(net_output_1)
-        if done:
-            break
-    print(time.time() - start)
+        env_run_id = env.step.remote(ps, net_output)
+        env_reset_id = env.reset.remote(ps)
+        ray.get([env_run_id, env_reset_id])
+        print(time.time() - start_time)
+
+    # Shutdown ray
+    ray.shutdown()
