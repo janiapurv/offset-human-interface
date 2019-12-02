0c17f96c00886b9e67f3187b63eecf81931c8800
Hemu <hemanthm2277@gmail.com>
2019-12-02 11:11:23 -0500

    Added complexity for the simulation

--- offset-game/main.py
+++ offset-game/main.py
@@ -6,6 +6,7 @@ import ray
 
 from server.parameters import ParameterServer
 from envs.environments import Benning
+from complexity.environments import ComplexBenning
 from gui.main import MainGUI
 
 from utils import skip_run
@@ -13,7 +14,7 @@ from utils import skip_run
 config_path = Path(__file__).parents[1] / 'offset-game/config.yml'
 config = yaml.load(open(str(config_path)), Loader=yaml.SafeLoader)
 
-with skip_run('run', 'Game Test') as check, check():
+with skip_run('skip', 'Game Test') as check, check():
 
     # Initiate ray
     if not ray.is_initialized():
@@ -31,6 +32,36 @@ with skip_run('run', 'Game Test') as check, check():
     gui_run_id = gui.run.remote(ps)
     env_run_id = env.step.remote(ps)
     ray.wait([env_run_id, gui_run_id])
+    print(time.time() - ray.get(gui.get_start_time.remote()))
+
+    # Shutdown ray
+    ray.shutdown()
+
+with skip_run('run', 'Complexity Test') as check, check():
+
+    # Initiate ray
+    if not ray.is_initialized():
+        ray.init(num_cpus=5)
+
+    # Instantiate parameter server
+    ps = ParameterServer.remote(config)
+
+    # Instantiate complex environment
+    complex_env = ComplexBenning.remote(config)
+
+    # Instantiate environment
+    env = Benning.remote(config)
+
+    # Instantiate GUI
+    gui = MainGUI.remote(config, (1500, 750), ps)
+
+    # Get the remote IDs
+    gui_run_id = gui.run.remote(ps)
+    env_run_id = env.step.remote(ps)
+    complex_run_id = complex_env.step.remote(ps)
+
+    # Run the simulation
+    ray.wait([env_run_id, complex_run_id, gui_run_id])
     print(time.time() - ray.get(gui.get_start_time.remote()))
 
     # Shutdown ray
